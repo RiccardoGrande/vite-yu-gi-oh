fdbe68479921b64e4deca24db89bcf342b3544bb
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-17 17:50:52 +0200

    add change

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,9 @@
 <script>
+import axios from 'axios'
 import { store } from '../store.js'
 
 export default {
+    name: "AppMain",
     data() {
         return {
             store
@@ -9,18 +11,17 @@ export default {
     },
     methods: {
         apiCall() {
-            console.log(store.searchText)
-            axios.post('/store', {
-                card_images: 'image_url',
-                name: '',
-                archetype: ''
-            })
+            //console.log(store.searchText)
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0')
                 .then((response) => {
                     console.log(response);
                 }, (error) => {
                     console.log(error);
                 });
         }
+    },
+    mounted() {
+        this.apiCall();
     }
 
 
@@ -32,12 +33,17 @@ export default {
         <section>
             <div class="container">
                 <div class="row">
+
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
                     <div class="col-12"></div>
                     <div class="col">
                         <div class="card" style="width: 18rem;">
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">{{ store.name }}</h5>
+                                <h5 class="card-title" v-for="item in store">{{ item.name }}</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk
                                     of the card's content.</p>
                                 <a href="#" class="btn btn-primary">Go somewhere</a>
@@ -52,4 +58,8 @@ export default {
 
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+    background-color: #d48f38;
+}
+</style>

bfac2c3af0e67540c2830a12c2ab246bf078bb40
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-18 15:07:11 +0200

    add change color bg

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,26 @@
 <script>
 import axios from 'axios'
 import { store } from '../store.js'
+import SearchBox from './SearchBox.vue'
 
 export default {
     name: "AppMain",
+    components: {
+        SearchBox
+    },
     data() {
         return {
             store
         }
     },
     methods: {
+        performSearch() {
+            console.log(store.searchURL)
+            const url = store.searchURL + `?name=${store.searchURL}`
+            console.log(url)
+
+
+        },
         apiCards() {
             //console.log(store.searchText)
             axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0')
@@ -25,6 +36,7 @@ export default {
     mounted() {
 
         this.apiCards()
+
     }
 
 
@@ -36,12 +48,14 @@ export default {
         <section>
             <div class="container">
                 <div class="row">
+                    <SearchBox @makeSearch="performSearch()"></SearchBox>
+
 
                 </div>
             </div>
             <div class="container">
-                <div class="row">
-                    <div class="col-12"></div>
+                <div class="row content">
+                    <div class="col-12">Founded {{ this.store.searchURL.lenght }} cards.</div>
                     <div class="col" v-for="card in store.cards">
                         <div class="card">
                             <img :src="card.card_images[0].image_url" class="card-img-top" alt="...">
@@ -63,5 +77,9 @@ export default {
 <style lang="scss" scoped>
 section {
     background-color: #d48f38;
+
+    .content {
+        background-color: #fff;
+    }
 }
 </style>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'SearchBox'
+}
+</script>
+
+
+<template>
+    <div class="searchbox">
+        <div class="input-group ">
+            <input type="text" class="form-control" placeholder="Search" @keyup="$emit('makeSearch')">
+            <button class="btn btn-outline-secondary" type="button" @click="$event => $emit('makeSearch')">Search</button>
+        </div>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped></style>

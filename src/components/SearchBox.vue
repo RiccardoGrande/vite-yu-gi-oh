59c97a99c54a8cc1ae9fe767f2847db40065e3a0
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-18 15:32:25 +0200

    add change script search

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -8,8 +8,9 @@ export default {
 <template>
     <div class="searchbox">
         <div class="input-group ">
-            <input type="text" class="form-control" placeholder="Search" @keyup="$emit('makeSearch')">
-            <button class="btn btn-outline-secondary" type="button" @click="$event => $emit('makeSearch')">Search</button>
+            <select name="text" class="form-control" id="" @keyup="$emit('makeSearch')"></select>
+
+
         </div>
     </div>
 </template>
